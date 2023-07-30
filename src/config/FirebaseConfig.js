0c8eada1111475aee95e9b208f2a1666f0817cb0
siva <raavibalasivadurgaprasad@gmail.com>
2023-07-30 15:45:09 +0530

    updated

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -1,4 +1,4 @@
-export default   {
+ const FirebaseConfig=   {
     apiKey: "AIzaSyAooCmsYJhSjORwyvj5aBvS7KRdFaBG2L8",
     authDomain: "mygitapp-863eb.firebaseapp.com",
     projectId: "mygitapp-863eb",
@@ -6,5 +6,5 @@ export default   {
     messagingSenderId: "65397304810",
     appId: "1:65397304810:web:b045282d757ad9b23a5693",
     measurementId: "G-H4ESQW5L5M"
-  };
-
+};
+export default FirebaseConfig
